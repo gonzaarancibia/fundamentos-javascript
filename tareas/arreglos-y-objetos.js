fc6adf4098493f69206d12de044999570e7a592e
teresallobell <teresa.llobell13@gmail.com>
2025-02-23 22:40:53 +0100

    dos tareas terminadas

--- tareas/arreglos-y-objetos.js
+++ tareas/arreglos-y-objetos.js
@@ -3,30 +3,40 @@
 // <---Arreglos y Objetos--->
 
 // Crea un arreglo vacío llamado 'frutas'
-let frutas;
-
+let frutas =[];
 // Agrega al menos 3 frutas al arreglo
+  frutas.push("manzana");
+  frutas.push("bananas");
+  frutas.push("naranja");
 
 // Crea un objeto llamado 'persona' con las propiedades: nombre, edad y ciudad
-let persona;
-
+  
+  let persona={
+    nombre:"teresa",
+    edad:53,
+    ciudad:"algeciras",
+   }
+   
 // Agrega una nueva propiedad al objeto 'persona': profesion
-
+persona.profesion= "cocinera"
 // <---Manipulación de arreglos --->
+    
 
 // Agrega un elemento al final del arreglo 'frutas'
+frutas.push("kiwi");
 
 // Elimina el primer elemento del arreglo 'frutas'
+frutas.shift();
 
 // Encuentra la longitud del arreglo 'frutas' y guárdala en una variable
-let longitudFrutas;
+let longitudFrutas= frutas.length
 
 // Manipulación de objetos
 // Accede a la propiedad 'nombre' del objeto 'persona' y guárdala en una variable
-let nombrePersona;
+let nombrePersona = persona.nombre;
 
 // Modifica la propiedad 'edad' de 'persona'
-
+persona.edad = 50;
 // Elimina la propiedad 'ciudad' del objeto 'persona'
 
 module.exports = {
--- tareas/intro.js
+++ tareas/intro.js
@@ -1,10 +1,16 @@
 
 //<--- Introducción a JavaScript --->
-
+//Es un lenguaje de programacion que se utiliza para hacer paginas web interactivas mas dinamica y funcional.
+//No necesita un servidor para usarlo puede crear aplicaciones webcompleta ,trabajar en tiempo real directamente desde el
+//  navegador del usuario
 
 
 //Escribe un comentario explicando qué es JavaScript
 // y para qué se usa.
 
+//js hace que las paginas web y moviles, dinamicas y atractivas .Desarollo de juegos y aplicaciones a servidores, valida formularios
+//antes deenviar datos al servidor,hace que las paginas de ser estaticas a dinamicas e interactivas .
 
 // Escribe un comentario explicando las diferencias entre JavaScript, HTML, CSS.
+//html define la estructura de la pagina,texto,imagenes, botones, formularios etc..
+//css le da estilo ,colores diseño, tamaños ,etc...
