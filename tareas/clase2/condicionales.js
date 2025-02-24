e7af5fca370d3bcbce2f06308eefbcccf636003e
gonzaarancibia <gonzaloarancibia48@gmail.com>
2025-02-24 07:39:41 -0300

    add condicionales practice

---
+++ tareas/clase2/condicionales.js
@@ -0,0 +1,37 @@
+// tareas/condicionales.js
+
+// Condicionales
+// Crea una variable 'numero' y asigna un valor numérico
+let numero;
+
+// Usa un condicional if para verificar si 'numero' es positivo, negativo o cero
+
+// Usa un condicional if-else para verificar si 'numero' es par o impar
+
+// Crea una variable 'edad' y usa un condicional para verificar si es mayor de edad (18 o más)
+let edad;
+
+// Usa un condicional if-else if-else para clasificar 'edad' en: 
+// - "Niño" si está entre 0 y 12 años
+// - "Adolescente" si está entre 13 y 17 años
+// - "Adulto" si está entre 18 y 64 años
+// - "Adulto mayor" si tiene 65 años o más
+
+// Usa un switch para determinar si un día de la semana (lunes a domingo) es un "Día laborable" (lunes a viernes) o "Fin de semana" (sábado y domingo)
+let diaSemana;
+
+// Crea una variable 'calificacion' que puede contener una letra (A, B, C, D, F). 
+// Usa un switch para asignar un mensaje descriptivo según la calificación:
+// - "A" -> "Excelente"
+// - "B" -> "Muy bien"
+// - "C" -> "Satisfactorio"
+// - "D" -> "Insuficiente"
+// - "F" -> "Reprobado"
+let calificacion;
+
+module.exports = {
+    numero,
+    edad,
+    diaSemana,
+    calificacion,
+};
